2d68be1037f53d03492cb0094e4bccf4b79e7473
Shadow <madion86@yandex.ru>
2018-08-19 08:12:37 +0300

    lesson 2 reworked

--- lesson-2/users.js
+++ lesson-2/users.js
@@ -25,14 +25,16 @@ document.querySelector('button').addEventListener('click', () => {
   let name = document.querySelector('input[aria-label="Username"]').value;
   let email = document.querySelector('input[aria-label="E-Mail"]').value;
   let age = document.querySelector('input[aria-label="Age"]').value;
-  if (user.name != undefined && user.email != undefined && user.age != undefined) {
+  if (name != undefined && email != undefined && age != undefined) {
+    let user = JSON.stringify({
+      name,
+      email,
+      age
+    });
+    console.log(user);
     fetch('http://89.108.65.123/user', {
       method: 'post',
-      body: {
-        name,
-        email,
-        age
-      }
+      body: user
     }).then(res => {
       console.log(res);
     }).catch(err => {
