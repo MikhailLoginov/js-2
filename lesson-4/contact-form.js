e0830a6d42086046c29ffb88a30f68b55ca7bccd
Shadow <madion86@yandex.ru>
2018-08-21 02:19:48 +0300

    dom manipulations reworked and removed

--- lesson-4/contact-form.js
+++ lesson-4/contact-form.js
@@ -2,47 +2,55 @@ new Vue({
   el: '#contact-form',
   data: {
     cities: [],
-    inputFocused: false
+    inputFocused: false,
+    name: "",
+    email: "",
+    phone: "",
+    cityName: "",
+    nameValError: "",
+    phoneValError: "",
+    emailValError: "",
+    messageClass: "",
+    messageText: ""
   },
   methods: {
     validateForm: function () {
-      const name = document.querySelector('#name').value,
-        phone = document.querySelector('#phone').value,
-        email = document.querySelector('#email').value;
-      if (/[^a-z^A-Z]/.test(name) || name == "") {
+      if (/[^a-z^A-Z]/.test(this.name) || this.name == "") {
         throw Error('Enter correct name');
       };
-      if (!/^\+\d\(\d{3}\)\d{3}\-\d{4}$/.test(phone) || phone == "") {
+      if (!/^\+\d\(\d{3}\)\d{3}\-\d{4}$/.test(this.phone) || this.phone == "") {
         throw Error('Enter correct phone');
       };
-      if (!/^\w{2,}(\.\w{2,})?(\-\w{2,})?\@[a-zA-Z_]{2,}\.[a-zA-Z]{2,}$/.test(email) || email == "") {
+      if (!/^\w{2,}(\.\w{2,})?(\-\w{2,})?\@[a-zA-Z_]{2,}\.[a-zA-Z]{2,}$/.test(this.email) || this.email == "") {
         throw Error('Enter correct email');
       }
     },
     validate: function () {
-      document.querySelector('#name').style.border = "1px solid #ced4da";
-      document.querySelector('#phone').style.border = "1px solid #ced4da";
-      document.querySelector('#email').style.border = "1px solid #ced4da";
+      this.nameValError = "";
+      this.phoneValError = "";
+      this.emailValError = "";
       try {
         this.validateForm();
-        let message = document.createElement('div');
-        message.className = `alert alert-success`;
-        message.innerText = "Success!";
-        document.body.insertBefore(message, document.querySelector('script'));
-        setTimeout(() => message.remove(), 5000);
+        this.messageClass = "alert alert-success";
+        this.messageText = "Success!";
+        setTimeout(() => {
+          this.messageClass = "";
+          this.messageText = "";
+        }, 5000);
       } catch (err) {
         if (err.message.indexOf("name") > 0) {
-          document.querySelector('#name').style.border = "1px solid red";
+          this.nameValError = "error";
         } else if (err.message.indexOf("phone") > 0) {
-          document.querySelector('#phone').style.border = "1px solid red";
+          this.phoneValError = "error";
         } else if (err.message.indexOf("email") > 0) {
-          document.querySelector('#email').style.border = "1px solid red";
+          this.emailValError = "error";
         }
-        let message = document.createElement('div');
-        message.className = `alert alert-danger`;
-        message.innerText = err;
-        document.body.insertBefore(message, document.querySelector('script'));
-        setTimeout(() => message.remove(), 5000);
+        this.messageClass = "alert alert-danger";
+        this.messageText = err.message;
+        setTimeout(() => {
+          this.messageClass = "";
+          this.messageText = "";
+        }, 5000);
       }
     },
     openDropdown: function () {
@@ -58,8 +66,7 @@ new Vue({
       }
     },
     searchCities: function () {
-      let value = document.querySelector('input[aria-label="Text input with dropdown button"]').value;
-      fetch(`https://api.teleport.org/api/cities/?search=${value}`).then(res => {
+      fetch(`https://api.teleport.org/api/cities/?search=${this.cityName}`).then(res => {
         return res.json();
       }).then(data => {
         this.cities = data._embedded["city:search-results"];
@@ -69,8 +76,8 @@ new Vue({
       }
     },
     choiceTheCity: function(city) {
-      document.querySelector('input[aria-label="Text input with dropdown button"]').value = city;
+      this.cityName = city;
+      this.inputFocused = false;
     },
-    
   }
 });
--- readme.md
+++ readme.md
@@ -1,35 +1,4 @@
 # GeekBrains JavaScript-2 Homeworks
 
-## Lesson 2
+## Lesson 4
 
-Задание из методички смотреть через menu.html. Меню доработано согласно заданиям. Структура получаемого для формирования меню JSON файла имеет следующий вид:
-
- * {
- *   li_id : {"href", "name"},
- *   li_id : {"href", "name"},
- *   submenu_ul_id : {
- *     submenu_li_id : {"href", "name"},
- *     ...
- *     }
- *   ...
- * }
-
-Добавлена возможность скрывать и раскрывать подменю, щелкая на них мышью (по умолчанию они у меня для наглядности раскрыты)
-
-При щелчке на пункт меню "Галерея" открывается фотогалерея (3е задание): обрабатывается JSON файл, который должен иметь следующую струтуру:
-
- * {
- *   pic_id : {
- *     name : "picture name",
- *     small_img : "path/to/small-img.jpg",
- *     full_img : "path/to/full-img.jpg"
- *   },
- *   ...
- * }
-
-Задание про REST API находится в файлах users.html и users.js
-
-
- ## Lesson 3
-
- Везде есть простенький UI с bootstrap, первые два задания - quote.html / quote.js, третье задание - contact-form.html / contact-form.js
